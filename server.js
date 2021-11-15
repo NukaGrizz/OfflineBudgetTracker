bf9cbc5263e9db46bcd43706e1f01ba54ffd33ef
NukaGrizz <nukagrizz@gmail.com>
2021-11-15 18:23:08 -0500

    add debug mongo to server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+const app = express();
+
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
-const app = express();
 
 app.use(logger("dev"));
 
@@ -20,6 +20,8 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+mongoose.set('debug', true);
 
 // routes
 app.use(require("./routes/api.js"));
